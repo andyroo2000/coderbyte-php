7e8037273f575781bd34a600b87ae408dd9ddcf8
Andrew Landry <andrewlandry@gmail.com>
2015-04-23 17:05:48 -0400

    create get_next_letter function

--- spec/LetterChangesSpec.php
+++ spec/LetterChangesSpec.php
@@ -7,8 +7,33 @@ use Prophecy\Argument;
 
 class LetterChangesSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_returns_bcd_for_abc()
     {
-        $this->shouldHaveType('LetterChanges');
+        $this->letter_changes('abc')->shouldReturn('bcd');
     }
+
+	function it_returns_gvO_Ujnft_for_fun_times()
+	{
+		$this->letter_changes('fun times!')->shouldReturn('gvO Ujnft!');
+	}
+
+	function  it_returns_b_for_a()
+	{
+		$this->get_next_letter('a')->shouldReturn('b');
+	}
+
+	function  it_returns_c_for_b()
+	{
+		$this->get_next_letter('b')->shouldReturn('c');
+	}
+
+	function  it_returns_g_for_f()
+	{
+		$this->get_next_letter('f')->shouldReturn('g');
+	}
+
+	function it_returns_8_for_8()
+	{
+		$this->get_next_letter('8')->shouldReturn("8");
+	}
 }
--- src/LetterChanges.php
+++ src/LetterChanges.php
@@ -1,5 +1,39 @@
 <?php
 
+/*
+ * Using the PHP language, have the function LetterChanges(str) take the str parameter
+being passed and modify it using the following algorithm. Replace every letter in
+the string with the letter following it in the alphabet (ie. c becomes d, z becomes a).
+Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this
+modified string.
+*/
+
 class LetterChanges
 {
+	const ALPHABET = "abcdefghijklmnopqrstuvqyza";
+	const VOWELS = "aeiou";
+
+	function letter_changes($string)
+	{
+		$characters = str_split($string);
+		$result = "";
+
+		foreach ($characters as $character) {
+			$result .= $this->get_next_letter($character);
+		}
+
+		return $result;
+	}
+
+	function get_next_letter($letter) {
+		$characters = str_split(self::ALPHABET);
+
+		if (!in_array($letter, $characters)) {
+			return $letter;
+		}
+
+		$letter_index = array_search($letter, $characters);
+
+		return $characters[$letter_index + 1];
+	}
 }
