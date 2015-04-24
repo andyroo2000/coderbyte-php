fbb7962310a372dec9d1f3d94a17aabab331ce72
Andrew Landry <andrewlandry@gmail.com>
2015-04-24 01:04:10 -0400

    solve FirstFactorial

--- spec/FirstFactorialSpec.php
+++ spec/FirstFactorialSpec.php
@@ -7,8 +7,18 @@ use Prophecy\Argument;
 
 class FirstFactorialSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_returns_24_for_4()
     {
-        $this->shouldHaveType('FirstFactorial');
+        $this->first_factorial(4)->shouldReturn(24);
+    }
+
+	function it_returns_40320_for_8()
+    {
+        $this->first_factorial(8)->shouldReturn(40320);
+    }
+
+	function it_returns_2_for_2()
+    {
+        $this->first_factorial(2)->shouldReturn(2);
     }
 }
--- src/FirstFactorial.php
+++ src/FirstFactorial.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * have the function FirstFactorial(num) take the num parameter being passed
+ * and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)).
+ * For the test cases, the range will be between 1 and 18.
+ */
+
+
 class FirstFactorial
 {
+	function first_factorial($number)
+	{
+		$result = 1;
+
+		if ($number == 1) {
+			return 1;
+		}
+
+		for ($i = $number; $i > 1; $i--) {
+			$result *= $i;
+		}
+
+		return $result;
+	}
 }
